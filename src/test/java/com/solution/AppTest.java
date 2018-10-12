9a5cba10eb07a7db0c36bbd9226e3c5a8c7407f8
grzesiekpwrwroc <grzegorz.jarus@gmail.com>
2018-10-12 16:04:39 +0200

    Add tests

--- src/test/java/com/solution/AppTest.java
+++ src/test/java/com/solution/AppTest.java
@@ -1,20 +1,102 @@
 package com.solution;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void totalQuantity_returns4() {
+        Base base = new Base();
+        //ReadFile rd = new ReadFile();
+        List<Order> list = new ArrayList<>();
+        list.add(new Order("1", 1, "Bułka", 1, 10));
+        list.add(new Order("1", 1, "Chleb", 2, 15));
+        list.add(new Order("1", 2, "Chleb", 5, 15));
+        list.add(new Order("2", 1, "Chleb", 1, 10));
+        base.setBase(list);
+        int result = new ReportTotalQuantity(base.getBase()).getQuantity();
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void averagePrice_returns20() {
+
+        Base base = new Base();
+
+        List<Order> list = new ArrayList<>();
+        list.add(new Order("1", 1, "Bułka", 1, 30));
+        list.add(new Order("1", 1, "Chleb", 2, 15));
+        list.add(new Order("1", 2, "Chleb", 5, 15));
+        list.add(new Order("2", 1, "Chleb", 1, 20));
+        base.setBase(list);
+        double result = new ReportAveragePrice(base.getBase()).getAveragePrice();
+        assertEquals(20, (int) result);
+    }
+
+    @Test
+    public void clientAveragePrice_returns15() {
+
+        Base base = new Base();
+
+        List<Order> list = new ArrayList<>();
+        list.add(new Order("1", 1, "Bułka", 1, 30));
+        list.add(new Order("1", 1, "Chleb", 2, 15));
+        list.add(new Order("1", 2, "Chleb", 5, 15));
+        list.add(new Order("2", 1, "Chleb", 1, 20));
+        base.setBase(list);
+        double result = new ReportClientAveragePrice(base.getBase(), new Client("1")).getClientAveragePrice();
+        assertEquals(15, (int) result);
+    }
+
+    @Test
+    public void clientTotalQuantity_returns3() {
+
+        Base base = new Base();
+
+        List<Order> list = new ArrayList<>();
+        list.add(new Order("1", 1, "Bułka", 1, 30));
+        list.add(new Order("1", 1, "Chleb", 2, 15));
+        list.add(new Order("1", 2, "Chleb", 5, 15));
+        list.add(new Order("2", 1, "Chleb", 1, 20));
+        base.setBase(list);
+        long result = new ReportClientTotalQuantity(base.getBase(), new Client("1")).getQuantity();
+        assertEquals(3, (int) result);
+    }
+
+    @Test
+    public void totalOrdersPrice_returns80() {
+
+        Base base = new Base();
+
+        List<Order> list = new ArrayList<>();
+        list.add(new Order("1", 1, "Bułka", 1, 30));
+        list.add(new Order("1", 1, "Chleb", 2, 15));
+        list.add(new Order("1", 2, "Chleb", 5, 15));
+        list.add(new Order("2", 1, "Chleb", 1, 20));
+        base.setBase(list);
+        double result = new ReportTotalOrdersPrice(base.getBase()).getTotalPrice();
+        assertEquals(80, (int) result);
+    }
+
+    @Test
+    public void clientTotalOrdersPrice_returns60() {
+
+        Base base = new Base();
+
+        List<Order> list = new ArrayList<>();
+        list.add(new Order("1", 1, "Bułka", 1, 30));
+        list.add(new Order("1", 1, "Chleb", 2, 15));
+        list.add(new Order("1", 2, "Chleb", 5, 15));
+        list.add(new Order("2", 1, "Chleb", 1, 20));
+        base.setBase(list);
+        double result = new ReportClientTotalOrdersPrice(base.getBase(), new Client("1")).getClientTotalPrice();
+        assertEquals(60, (int) result);
     }
 }
