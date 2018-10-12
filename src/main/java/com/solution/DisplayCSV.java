45d370e1240098a174b229d1301a0fe90eb0ed46
grzesiekpwrwroc <grzegorz.jarus@gmail.com>
2018-10-12 16:06:21 +0200

    Add report display

--- src/main/java/com/solution/DisplayCSV.java
+++ src/main/java/com/solution/DisplayCSV.java
@@ -1,5 +1,19 @@
 package com.solution;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class DisplayCSV {
+
+
+    public static void createReport(Report report) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new File("Report.csv"));
+        StringBuilder sb = new StringBuilder();
+        sb.append(report);
+        pw.write(sb.toString());
+        pw.close();
+    }
+
 }
--- src/main/java/com/solution/DisplayScreen.java
+++ src/main/java/com/solution/DisplayScreen.java
@@ -2,4 +2,8 @@ package com.solution;
 
 
 public class DisplayScreen {
+
+    public static void createReport(Report report){
+        System.out.println(report.toString());
+    }
 }
